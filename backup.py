b6a61b051a0d7a71f4c7a88f29e18219c43046e1
Zach Biles <zacharybiles@gmail.com>
2020-06-22 10:35:27 -0500

    initial connection working

--- backup.py
+++ backup.py
@@ -2,20 +2,24 @@ from netmiko import ConnectHandler
 from getpass import getpass
 import re
 
-ip_addrs_file = open('ips.txt')
-devices = ip_addrs_file.read().splitlines()
+#ip_addrs_file = open('ips.txt')
+#devices = ip_addrs_file.read().splitlines()
+devices = ['192.168.3.20']
 
 for ip in devices:
     device = {
         'device_type': 'mikrotik_routeros',
         'ip': ip,
+        'port': 22,
         'username': 'ansible',
         #'password': getpass(),
         #'password': 'ansible'
-        'key_filename': ''
+        'use_keys': True,
+        'key_file': "~\\.ssh\\ansible_rsa"
     }
     net_connect = ConnectHandler(**device)
     net_connect.enable()
-    #print(net_connect.find_prompt())
 
 net_connect = ConnectHandler(**device)
+net_connect.enable()
+print(net_connect.find_prompt())
